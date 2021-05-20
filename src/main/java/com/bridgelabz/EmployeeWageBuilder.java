ee4a84aed9da6b5db140aa8a5320226694854043
anupama cv <anu.anupamacv@gmail.com>
2021-05-20 16:00:44 +0530

    refactored daily employee wage computation

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,19 +1,26 @@
 package com.bridgelabz;
 public class EmployeeWageBuilder {
     public static void main(String[] arg) {
-        final int IS_PRESENT=1;
+        final int IS_FULL_TIME=1;
+        final int IS_PART_TIME=2;
         final int WAGE_PER_HOUR=20;
         final int FULL_DAY_HOUR=8;
-        double Employee_Check=Math.floor((Math.random() * 10)) % 2;
-        if (Employee_Check == IS_PRESENT)
+        final int PART_TIME_HOUR=4;
+        double Employee_Check=Math.floor((Math.random() * 10)) % 3;
+        if (Employee_Check == IS_FULL_TIME)
         {
-            System.out.println("Employee is present");
+            System.out.println("Employee is full time present");
             double Daily_Employee_Wage=(WAGE_PER_HOUR * FULL_DAY_HOUR);
             System.out.println("Daily employee wage is = " + Daily_Employee_Wage);
         }
-        else
+        else if (Employee_Check == IS_PART_TIME)
         {
-            System.out.println("Employee is absent");
+            System.out.println("Employee is Part time present");
+            double Daily_Employee_Wage=(WAGE_PER_HOUR * PART_TIME_HOUR);
+            System.out.println("Daily employee wage is = " + Daily_Employee_Wage);
+        }
+        else {
+            System.out.println(" Employee is absent ");
         }
     }
 }
