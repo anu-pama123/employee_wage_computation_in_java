53fc73dc418188a123d683519f78dd1af1359255
anupama cv <anu.anupamacv@gmail.com>
2021-05-20 18:26:00 +0530

    ADDED: functionality of calculating wages till a condition reached

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,11 +3,15 @@ package com.bridgelabz;
         public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
         public static final int WAGE_PER_HOUR= 20;
+        public static final int MAX_HOURS_IN_MONTH= 10;
+        public static final int NO_OF_WORKING_DAYS= 20;
     public static void main(String[] arg) {
         int total_salary = 0;
         int employee_hrs=0;
-        for (int day = 1; day <= 20; day++) {
+        int total_working_days=0;
+        while (total_working_days<=NO_OF_WORKING_DAYS  && employee_hrs<=MAX_HOURS_IN_MONTH) {
             int employee_check = (int) (Math.floor((Math.random() * 10)) % 3);
+            total_working_days++;
             switch (employee_check) {
                 case IS_FULL_TIME :
                     employee_hrs = 8;
