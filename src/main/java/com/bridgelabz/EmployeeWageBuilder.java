0c261629b3953a9aa8fdfc599a8e8c7ba3275f3f
anupama cv <anu.anupamacv@gmail.com>
2021-05-20 15:14:09 +0530

    ADDED: welcome message

---
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+public class EmployeeWageBuilder {
+    public static void main(String[] arg) {
+        System.out.println("Welcome to employee wage computation");
+    }
+}
